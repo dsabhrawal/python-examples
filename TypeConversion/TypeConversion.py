14d4a379727e810fb97bd3bbb8a9bb30be61a41a
dsabhrawal <deepak.sabhrawal@gmail.com>
2020-03-01 08:05:59 +0530

    TypeConversion example

---
+++ TypeConversion/TypeConversion.py
@@ -0,0 +1,15 @@
+#This examples talks about int() float() bool() str() complex() conversion functions 
+
+#int()
+x = 10.5
+print(int(x))  #prints 10
+
+#float()
+y = 7
+print(float(y))   #prints 7.0
+
+#bool()
+print(bool(y))  #print True
+print(bool(x)) #print True
+print(bool(0)) #Print False
+
