bb05a7c0e1acaddeee1190db9ed781cfbb7dbf97
dsabhrawal <deepak.sabhrawal@gmail.com>
2020-03-01 11:34:19 +0530

    Operators example

---
+++ Operators/BitwiseOperator.py
@@ -0,0 +1,38 @@
+# & (AND), | (OR), ~ (Compliment), ^ (XOR), << (Left Shift) , >> (Right Shift) 
+#Bitwise operators only works on int in binary form
+
+# AND (&) Table
+# 1 & 1 = 1
+# 1 & 0 = 0
+# 0 & 1 = 0
+# 0 & 0 = 0
+
+# OR (|) Table
+# 1 & 1 = 1
+# 1 & 0 = 1
+# 0 & 1 = 1
+# 0 & 0 = 0
+
+# XOR (^) Table
+# 1 & 1 = 0
+# 1 & 0 = 1
+# 0 & 1 = 1
+# 0 & 0 = 0
+
+# Compliment (~)
+# ~1 = 0
+# ~0 = 1
+
+print(7 & 19) #3
+print(7 | 19) #23
+print(~7) 
+
+print(bin(3))  #0b11
+print(bin(7))   # 0b111
+print(bin(19))   # 0b10011
+print(bin(23)) #0b10111
+
+
+
+
+
