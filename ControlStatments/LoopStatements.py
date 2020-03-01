d69629f539a8e40f551f6936d5a75b99b95de29b
Deepak Sabhrawal <deepak.sabhrawal@gmail.com>
2020-03-01 21:03:00 +0530

    control statemnts

--- ControlStatments/LoopStatements.py
+++ ControlStatments/LoopStatements.py
@@ -1,1 +1,44 @@
 # loop statements in Python
+
+
+#Print numbers 1 to 10
+
+x = 1
+while x<=10:
+    print(x)
+    x +=1
+print('End of Program!')
+
+
+#For loop
+
+for char in 'John':
+    print(char) #Each character is printed
+
+print()
+print()
+
+a=range(10)
+for i in a:
+    print(i)  #Prints number 0-9
+print()
+
+#Prints 10 to 1 numbers
+k = range(10,0,-1)
+for z in k: 
+    print(z)
+
+print()
+print()
+#Break and continue
+
+#Find first odd number in the given list
+list = [10,50,11,12,17]
+for i in list:
+    if(i%2 == 0):
+        continue
+    print(i)
+    break
+
+#Result: 11
+
---
+++ DataTypes/Range.py
@@ -0,0 +1,13 @@
+#buil in range()
+#sequence generated through range function is immutable
+
+a = range(10)
+print(a)  # range(0, 10)
+print(type(a)) #<class 'range'>
+
+print(range(0,10)) #range(0, 10)
+
+print(a[5]) #5
+
+print(range(0,10,2)) #range -- 0,2,4,6,8
+
