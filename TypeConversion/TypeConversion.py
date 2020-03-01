9b838743f6dffb602cb6852f193e9838c8d8eacc
dsabhrawal <deepak.sabhrawal@gmail.com>
2020-03-01 08:58:39 +0530

    TypeConversion example

--- TypeConversion/TypeConversion.py
+++ TypeConversion/TypeConversion.py
@@ -13,3 +13,11 @@ print(bool(y))  #print True
 print(bool(x)) #print True
 print(bool(0)) #Print False
 
+#str()
+print(str('10.5555'))
+print(str(x))
+
+#complex()
+print(complex(2))  #prints (2+0j)
+print(complex(2,1)) #prints (2+1j)
+print(complex(x))  #prints (10.5+0j)
